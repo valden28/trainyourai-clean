ef75492287bf74fd7448904525539d420c7bd4d6
valden28 <dennisvalentino@danddrestaurants.net>
2025-05-10 19:41:07 -0400

    Moved Auth0 login/logout/callback to pages/api for full App Router compatibility

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,4 +1,4 @@
 // app/api/auth/callback/route.ts
 import { handleCallback } from '@auth0/nextjs-auth0'
 
-export const GET = handleCallback()
+export default handleCallback()
--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,4 +1,4 @@
 // app/api/auth/logout/route.ts
 import { handleLogout } from '@auth0/nextjs-auth0'
 
-export const GET = handleLogout()
+export default handleLogout()
