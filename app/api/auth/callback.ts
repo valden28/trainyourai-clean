c6ba158c3efc879ede383aa669fc1479af18eca4
valden28 <dennisvalentino@danddrestaurants.net>
2025-05-10 19:51:40 -0400

    Fix: Add required config object to Auth0 handlers

--- app/api/auth/callback.ts
+++ app/api/auth/callback.ts
@@ -1,3 +1,3 @@
 import { handleCallback } from '@auth0/nextjs-auth0'
 
-export default handleCallback()
+export default handleCallback({})
--- app/api/auth/login.ts
+++ app/api/auth/login.ts
@@ -1,3 +1,3 @@
 import { handleLogin } from '@auth0/nextjs-auth0'
 
-export default handleLogin()
+export default handleLogin({})
