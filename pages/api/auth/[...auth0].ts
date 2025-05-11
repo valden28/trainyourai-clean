e340f899d062aaec60dcd72ee3e91a3f93c563e6
valden28 <dennisvalentino@danddrestaurants.net>
2025-05-11 13:06:00 -0400

    Fix: Move Auth0 route to /api/auth/[...auth0].ts

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,7 +1,5 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
 export default handleAuth({
-  login: handleLogin({
-    returnTo: '/chat-core',
-  }),
+  login: handleLogin({ returnTo: '/chat-core' }),
 });
