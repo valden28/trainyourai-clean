f675556075b60bda85dd80357d20448fbc3d1e3b
valden28 <dennisvalentino@danddrestaurants.net>
2025-05-12 21:43:20 -0400

    Fix Clerk middleware import (remove invalid authMiddleware)

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
-import { authMiddleware } from '@clerk/nextjs';
 
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)'], // Apply Clerk to all routes except static files
+  matcher: ['/((?!.*\\..*|_next).*)'], // Applies to all routes except static and _next
 };
